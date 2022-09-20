f066bc918cc24aa053964ef0f488a0ba868c3027
cocoabun <105182251+cocoabun@users.noreply.github.com>
2022-09-21 09:33:32 +1200

    create front end

--- app.js
+++ app.js
@@ -25,7 +25,16 @@ app.use('/api/articles', articles);
 
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
+//heroku
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
