966457917ca13721bd45caef183dfb34e443a25f
QinChen2020 <83799497+QinChen2020@users.noreply.github.com>
2022-09-25 19:28:12 +1300

    summary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  Route,
+  Routes,
+  NavLink,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
+import ArticleSummary from './pages/ArticleSummary';
+
+const App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <div>
+        <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
+          <ul className="header">
+          <li><NavLink exact to = "/">ArticleSummary</NavLink></li>
+              <li><NavLink to = "/ArticleSummary">Select the Practice</NavLink></li>
+             
+          </ul>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<ArticleSummary/>}/>
+            <Route  path="/ArticleSummary" element={<ArticleSummary/>}/>
+            
+          </Routes>
+
+        </div>
+      </div>
+      </Router>
   );
 }
 
