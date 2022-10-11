e31ebb14385c2aeb5677eea4c1df0dc5bcb32d68
QinChen2020 <83799497+QinChen2020@users.noreply.github.com>
2022-10-11 15:46:02 +1300

    add

--- frontend/src/components/ArticleSummary.js
+++ frontend/src/components/ArticleSummary.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import Styles from "../components/tablestyle.js";
@@ -58,58 +57,3 @@ const ArticleList = () => {
     )
 }
 export default ArticleList;
-=======
-//import { articles } from "../../../routes/api/articles.js";
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-// import BookCard from "./BookCard";
-import ArticlesCard from "./ArticleCard"
-// import router from "../../../routes/api/articles.js";
-
-
-//show article details
-const ArticleSummary = () => {
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-      let subscribe = true;
-        axios.get('http://localhost:5000/summary' )
-          .then(({data}) => {
-              if (subscribe) {
-                setArticles(() => data);
-              }
-          })
-          .catch(err => {
-              alert(err);
-          });
-      return () => {
-          subscribe = false;
-      }
-  });
-
-  const ArticlesCardData = () => {
-      return articles.map((res, i) => {
-          return <ArticlesCard object={res} key={i}/>;
-      })
-  };
-
-  return (
-      <>
-          <div className="ShowArticlesList">
-                  <div className="row">
-                      <div className="col-md-12">
-                          <br/>
-                          <h2 className="display-4 text-center">Articles Summary</h2>
-                      </div>
-                  </div>
-
-                  <div className="list">
-                      {ArticlesCardData()}
-                  </div>
-          </div>
-      </>
-  )
-}
-
-export default ArticleSummary;
-
->>>>>>> 423e109056d279e5bba2532820e716c701d6c9fa
